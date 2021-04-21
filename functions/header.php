bcc7da950dd6e37eafb93aa8fea817458cf55195
Unknown <felipe.fugimoto3@gmail.com>
2021-04-21 17:59:56 -0300

    construído o css, icones e feito uma parte do menu

--- functions/header.php
+++ functions/header.php
@@ -5,5 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forte Vidraçaria</title>
+     <!--Import Google Icon Font-->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php include_once 'functions/header.php'; ?>
 
-<div id="teste"></div>
-<script>
-var testar= document.getElementById('teste');
-testar.innerHtml="oi"
-document.
+<input type="checkbox" id="chec">
+<label for="chec"><img src="icones/botao-de-menu-de-tres-linhas-horizontais.png" alt="linhas"></label>
+<nav class="menu">
+<ul>
+    <li><a href="#">Inicio</a></li>
+    <li><a href="#">Gerar orçamentos</a></li>
+    <li><a href="#">Orçamentos gerados</a></li>
+    <li><a href="#">Cadastro</a></li>
+    <li><a href="#">Configuração</a></li>
+    
+</ul>
 
-
-</script>
+</nav>
 
 <?php include_once 'functions/footer.php'; ?>
